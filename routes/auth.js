25fc4ab4446b1048267031f703865ceb1deeaa75
j-wang-08 <jaywang08@outlook.com>
2024-05-23 01:30:01 -0700

    fix issue

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ const { login, register } = require("../controllers/authController");
 // login route
 router.post("/login", login);
 // register route
-router.post("register", register);
+router.post("/register", register);
 
 module.exports = router;
